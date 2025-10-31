8112c5889fb1b5d25a1a5ce356f73507b0a55e8f
kystarniknikita <nikitakyst@yandex.by>
2025-10-31 13:13:27 +0300

    ci resolving

--- UserService/src/test/java/com/example/UserService/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/UserService/UserServiceApplicationTests.java
@@ -1,8 +1,9 @@
 package com.example.UserService;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Tag("integration")
 @SpringBootTest
 class UserServiceApplicationTests {
 
