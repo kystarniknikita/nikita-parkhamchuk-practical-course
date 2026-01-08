44c6f055337dc1f107f65840aae16f2acef41681
kystarniknikita <nikitakyst@yandex.by>
2026-01-08 16:26:32 +0300

    fix

--- UserService/src/main/java/com/example/UserService/controller/UserController.java
+++ UserService/src/main/java/com/example/UserService/controller/UserController.java
@@ -7,7 +7,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,37 +21,37 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<UserResponse> create(@RequestBody @Valid UserRequest request) {
-        System.out.println("controller");
+        System.out.println("user controller");
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(userService.create(request));
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
+    @PreAuthorize("@authService.hasAdminRole(authentication) or @authService.isSelf(#id, authentication)")
     public ResponseEntity<UserResponse> getById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.findById(id));
     }
 
     @GetMapping
-//    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("@authService.hasAdminRole(authentication)")
     public ResponseEntity<List<UserResponse>> getAll() {
         return ResponseEntity.ok(userService.findAll());
     }
 
     @GetMapping("/email")
-//    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
+    @PreAuthorize("@authService.hasAdminRole(authentication) or authentication.name == #email")
     public ResponseEntity<UserResponse> getByEmail(@RequestParam String email) {
         return ResponseEntity.ok(userService.findByEmail(email));
     }
 
     @PutMapping("/{id}")
-//    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
+    @PreAuthorize("@authService.hasAdminRole(authentication) or @authService.isSelf(#id, authentication)")
     public ResponseEntity<UserResponse> updateById(@PathVariable Long id, @RequestBody @Valid UserRequest request) {
         return ResponseEntity.ok(userService.update(id, request));
     }
 
     @DeleteMapping("/{id}")
-//    @PreAuthorize("hasRole('ADMIN') or #id == authentication.principal.id")
+    @PreAuthorize("@authService.hasAdminRole(authentication) or @authService.isSelf(#id, authentication)")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         userService.deleteById(id);
 
